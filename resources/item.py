ebb51fbcf7678a399ea7a72310cabc10981d950b
Oluwafemi Sosami <saphemmy@gmail.com>
2023-07-09 16:23:04 +0100

    Perform cleaner Data Validation with Marshmallow

--- resources/item.py
+++ resources/item.py
@@ -4,6 +4,7 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
 from db import items
+from schemas import ItemSchema, ItemUpdateSchema
 
 blp = Blueprint("Items", "items", description="Operation on the items")
 
@@ -16,31 +17,6 @@ class Item(MethodView):
         except KeyError:
             abort(404, message="Item not found.")
 
-    def post(self):
-        item_data = request.get_json()
-
-        if (
-            "price" not in item_data
-            or "store_id" not in item_data
-            or "name" not in item_data
-        ):
-            abort(
-                400,
-                message="Bad request. Ensure 'price', 'store_id', and 'name' is included in the Payload"
-            )
-        for item in items.values():
-            if (
-                item_data["name"] == item["name"]
-                and item_data["store_id"] == item["store_id"]
-            ):
-                abort(400, message=f"Item already exists.")
-
-        item_id = uuid.uuid4().hex
-        item = {**item_data, "id": item_id}
-        items[item_id] = item
-
-        return item
-
     def delete(self, item_id):
         try:
             del items[item_id]
@@ -49,12 +25,8 @@ class Item(MethodView):
         except KeyError:
             abort(404, message="Item not found.")
 
-    def put(self, item_id):
-        item_data = request.get_json()
-
-        if "price" not in item_data or "name" not in item_data:
-          abort(400, message="Bad Request. ENsure 'price', and 'name' are included in the payload")
-
+    @blp.arguments(ItemUpdateSchema)
+    def put(self, item_data, item_id):
         try:
             item = items[item_id]
 
@@ -63,3 +35,24 @@ class Item(MethodView):
             return item
         except KeyError:
             abort(404, message="Item not found.")
+
+
+@blp.route("/item")
+class ItemList(MethodView):
+        def get(self):
+            return {"items": list(items.values())}
+        
+        @blp.arguments(ItemSchema)
+        def post(self, item_data):
+            for item in items.values():
+                if (
+                    item_data["name"] == item["name"]
+                    and item_data["store_id"] == item["store_id"]
+                ):
+                    abort(400, message=f"Item already exists.")
+
+            item_id = uuid.uuid4().hex
+            item = {**item_data, "id": item_id}
+            items[item_id] = item
+
+            return item
